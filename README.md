4856ff18af9e5eef503b211e4a670b6293ed9bd4
Core <83958883+Core-Quest@users.noreply.github.com>
2022-10-03 16:14:08 -0400

    Fix GAP v1.3.0

--- README.md
+++ README.md
@@ -1,20 +1,6 @@
-# More By Core
+# #More By Core
 
 Mod that add lots of setting to the game to make it more fun for you!
-
-## Features
-* Saber Marq Qeeper(Keeps your BurnMarks)/n
-* NoPromo(Removes Annoying Promotion)
-* Game Color(Gives you the ability to switch the background color in the whole game!)
-#### New Stuff in v1.2.0
-* No Particles(Removes saber Particles from bloqs)
-* No Bomb Effects(Removes Particles from Bombs when hit!)
-* Pause Menu Game Color Switcher(Allows for switching Game Color in the Pause Menu)
-* Better UI(Cleaner/Easier to use UI)
-
-## Known Bugs
-* Pause Menu Game Color Switcher(Doesnt Update Color unless reset song from pause menu!)
-* Main UI leaves GAP (Yes I am working on a fix)
 
 ## Credits
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "main.hpp"
+using namespace MainUI;
 #include "MainConfig.hpp"
 #include "questui/shared/QuestUI.hpp"
 #include "GlobalNamespace/MainMenuViewController.hpp"
@@ -27,12 +28,30 @@
 #include "UnityEngine/SceneManagement/SceneManager.hpp"
 #include "UnityEngine/SceneManagement/Scene.hpp"
 #include "GlobalNamespace/HealthWarningViewController.hpp"
+#include "TMPro/TextAlignmentOptions.hpp"
+#include "UnityEngine/TextAnchor.hpp"
+#include "UnityEngine/UI/LayoutElement.hpp"
+#include "System/Type.hpp"
+#include "HMUI/ImageView.hpp"
+#include "GlobalNamespace/GameCoreSceneSetup.hpp"
+#include "GlobalNamespace/StandardLevelDetailViewController.hpp"
+#include "GlobalNamespace/ColorSchemeColorToggleController.hpp"
+#include "GlobalNamespace/PlayerDataModel.hpp"
+#include "GlobalNamespace/PlayerData.hpp"
+#include "GlobalNamespace/ColorSchemesSettings.hpp"
+
 using namespace GlobalNamespace;
 using namespace QuestUI;
 using namespace custom_types;
 using namespace QuestUI::BeatSaberUI;
 
-
+#include "TMPro/TextMeshProUGUI.hpp"
+#include "UnityEngine/RectOffset.hpp"
+#include "GlobalNamespace/BombExplosionEffect.hpp"
+#include "GlobalNamespace/NoteCutParticlesEffect.hpp"
+#include "GlobalNamespace/PauseMenuManager.hpp"
+#include "GlobalNamespace/BombNoteController.hpp"
+#include "GlobalNamespace/MusicPackPromoBanner.hpp"
 static ModInfo modInfo;
 /// Define Our Config
 DEFINE_CONFIG(MainConfig);
@@ -45,31 +64,66 @@ Logger& getLogger() {
 float preview_acc;
 bool updatelights;
 GlobalNamespace::MenuLightsManager *menuLightsmanager;
+using namespace UnityEngine;
 
-void DidActivate(HMUI::ViewController* self, bool firstActivation, bool addedtoHierarchy, bool screenSystemEnabling) {
+
+
+int getFontStyleValue(Il2CppObject* self){
+	int fontStyle = CRASH_UNLESS(il2cpp_utils::GetPropertyValue<int>(self, "fontStyle"));
+	if (fontStyle & (1 << 1)) {
+		return fontStyle - 2;
+	}
+	else {
+		return -1;
+	}
+}
+
+DEFINE_TYPE(MainUI, MoreMainUI);
+GameObject *mainCont;
+GameObject *saberCont;
+GameObject *colorCont;
+GameObject *pauseClrCont;
+
+bool screensEnabled = false;
+void MoreMainUI::DidActivate(bool firstActivation, bool addedtoHierarchy, bool screenSystemEnabling) {
     using namespace UnityEngine;
+    using namespace TMPro;
+    using namespace UnityEngine::UI;
 
     if(firstActivation)
     {
-        GameObject* container = QuestUI::BeatSaberUI::CreateScrollableSettingsContainer(self->get_transform());
-        UnityEngine::UI::VerticalLayoutGroup* Vertical = QuestUI::BeatSaberUI::CreateVerticalLayoutGroup(self->get_transform());
-        UnityEngine::UI::HorizontalLayoutGroup* Horizontal = QuestUI::BeatSaberUI::CreateHorizontalLayoutGroup(self->get_transform());
-
-        QuestUI::BeatSaberUI::CreateUIButton(container->get_transform(), "Made By Core");
-        QuestUI::BeatSaberUI::CreateText(container->get_transform(), "");
-        QuestUI::BeatSaberUI::CreateUIButton(container->get_transform(), "Menu/(In)Game Settings");
-        QuestUI::BeatSaberUI::CreateText(container->get_transform(), "_______________________________________________");
-        AddConfigValueToggle(container->get_transform(), getMainConfig().isSaberMarqEnabled);
-        AddConfigValueToggle(container->get_transform(), getMainConfig().NoPromo);
-        QuestUI::BeatSaberUI::CreateText(container->get_transform(), "_______________________________________________");
-        QuestUI::BeatSaberUI::CreateText(container->get_transform(), "");
-        QuestUI::BeatSaberUI::CreateUIButton(container->get_transform(), "Menu Light Config");
-        QuestUI::BeatSaberUI::CreateText(container->get_transform(), "_______________________________________________");
-        AddConfigValueModifierButton(container->get_transform(), getMainConfig().lightsEnable);
+        mainCont = QuestUI::BeatSaberUI::CreateScrollableSettingsContainer(get_transform());
+        saberCont = QuestUI::BeatSaberUI::CreateFloatingScreen(Vector2(80.0f, 100.f), Vector3(-3.5f, 1.0f, 3.5f), Vector3(0.0f, -40.0f, 0.0f), 0.0f, true, false);
+        colorCont = QuestUI::BeatSaberUI::CreateFloatingScreen(Vector2(80.0f, 100.f), Vector3(3.5f, 1.0f, 3.5f), Vector3(0.0f, 40.0f, 0.0f), 0.0f, true, false);
+        UnityEngine::UI::VerticalLayoutGroup* vertClrCont = QuestUI::BeatSaberUI::CreateVerticalLayoutGroup(colorCont->get_transform());
 
         
 
-        auto colorPicker = BeatSaberUI::CreateColorPicker(container->get_transform(), "Light Color Changer", getMainConfig().MenuClr.GetValue(),[](UnityEngine::Color color) {
+
+
+
+
+        QuestUI::BeatSaberUI::CreateText(mainCont->get_transform(), "");
+        QuestUI::BeatSaberUI::CreateUIButton(mainCont->get_transform(), "Main Configs", UnityEngine::Vector2(0.0f, 90.0f),UnityEngine::Vector2(79.0f, 10.0f));
+
+        AddConfigValueToggle(mainCont->get_transform(), getMainConfig().NoPromo);
+        AddConfigValueToggle(mainCont->get_transform(), getMainConfig().NoParticles);
+        AddConfigValueToggle(mainCont->get_transform(), getMainConfig().NoBombs);
+
+
+
+        QuestUI::BeatSaberUI::CreateUIButton(saberCont->get_transform(), "Saber Configs", UnityEngine::Vector2(0.0f, 54.0f),UnityEngine::Vector2(79.9f, 10.0f));
+        AddConfigValueToggle(saberCont->get_transform(), getMainConfig().isSaberMarqEnabled);
+        
+
+
+
+        QuestUI::BeatSaberUI::CreateUIButton(colorCont->get_transform(), "Menu Light Config", UnityEngine::Vector2(0.0f, 54.0f),UnityEngine::Vector2(79.9f, 10.0f));
+        AddConfigValueToggle(colorCont->get_transform(), getMainConfig().lightsEnable);
+
+        
+
+        auto colorPicker = BeatSaberUI::CreateColorPicker(colorCont->get_transform(), "Light Color", getMainConfig().MenuClr.GetValue(),[](UnityEngine::Color color) {
             getMainConfig().MenuClr.SetValue(color, true);
             preview_acc = 0;
             (getMainConfig().updatelights_preview.SetValue(true)); 
@@ -77,7 +131,7 @@ void DidActivate(HMUI::ViewController* self, bool firstActivation, bool addedtoH
             (getMainConfig().updatelights_preview.SetValue(false));
         });
 
-        QuestUI::BeatSaberUI::CreateUIButton(colorPicker->get_transform(), "Refresh",UnityEngine::Vector2(0,+0.1),UnityEngine::Vector2(30,8),
+        QuestUI::BeatSaberUI::CreateUIButton(colorPicker->get_transform(), "Refresh",UnityEngine::Vector2(0,+0.3),UnityEngine::Vector2(30,8),
         [colorPicker]()
             {
                 getMainConfig().MenuClr.SetValue(UnityEngine::Color(5.0, 5.0, 5.0, 5.0));
@@ -87,13 +141,98 @@ void DidActivate(HMUI::ViewController* self, bool firstActivation, bool addedtoH
             });
         
         UnityEngine::Resources::FindObjectsOfTypeAll<GlobalNamespace::MenuLightsManager*>().First()->RefreshColors();
-        QuestUI::BeatSaberUI::CreateText(container->get_transform(), "*Note: Refresh Resets Colors/Enables(Disables)!");
+        QuestUI::BeatSaberUI::CreateText(colorCont->get_transform(), "*Note: Refresh Resets Colors/Enables(Disables)!");
+        
+        
+
+    }
+    if (!screensEnabled) {
+        screensEnabled = true;
+        mainCont->SetActive(true);
+        saberCont->SetActive(true);
+        colorCont->SetActive(true);
+ 
+        
     }
 }
 
+void MoreMainUI::DidDeactivate(bool removedFromHierarchy, bool screenSystemDisabling) {
+    screensEnabled = false;
+    mainCont->SetActive(false);
+    saberCont->SetActive(false);
+    colorCont->SetActive(false);
+ 
+   
 
+}
 
+MAKE_HOOK_MATCH(PauseMenuManager_ShowMenu, &PauseMenuManager::ShowMenu, void, PauseMenuManager *self){
+    PauseMenuManager_ShowMenu(self);
 
+    if (getMainConfig().lightsEnable.GetValue()){
+        pauseClrCont = BeatSaberUI::CreateFloatingScreen(Vector2(70.0f, 70.0f), Vector3(1.0f, 2.85f, 2.4f), Vector3(-30.0f, 0.0f, 0.0f), 0.0f, false, false);
+
+        auto colorPicker = BeatSaberUI::CreateColorPicker(pauseClrCont->get_transform(), "Light Color", getMainConfig().MenuClr.GetValue(),[](UnityEngine::Color color) {
+            getMainConfig().MenuClr.SetValue(color, true);
+            preview_acc = 0;
+            (getMainConfig().updatelights_preview.SetValue(true)); 
+            UnityEngine::Resources::FindObjectsOfTypeAll<GlobalNamespace::MenuLightsManager*>().First()->RefreshColors();
+            (getMainConfig().updatelights_preview.SetValue(false));
+        });
+
+        QuestUI::BeatSaberUI::CreateUIButton(colorPicker->get_transform(), "Refresh",UnityEngine::Vector2(0,+0.3),UnityEngine::Vector2(20,8),
+        [colorPicker]()
+            {
+                getMainConfig().MenuClr.SetValue(UnityEngine::Color(5.0, 5.0, 5.0, 5.0));
+                colorPicker->set_currentColor(getMainConfig().MenuClr.GetValue());
+                UnityEngine::Resources::FindObjectsOfTypeAll<GlobalNamespace::MenuLightsManager*>().First()->RefreshColors();
+                preview_acc = 0;
+            });
+        
+        UnityEngine::Resources::FindObjectsOfTypeAll<GlobalNamespace::MenuLightsManager*>().First()->RefreshColors();
+        if (!screensEnabled)
+        {
+        getLogger().info("Setting screen to active");
+        pauseClrCont->SetActive(true);
+        }
+    }
+}
+
+MAKE_HOOK_MATCH(PauseMenuManager_ContinueButtonPressed, &PauseMenuManager::ContinueButtonPressed, void, PauseMenuManager *self) {
+    PauseMenuManager_ContinueButtonPressed(self);
+    
+    if (!getMainConfig().lightsEnable.GetValue())
+    {
+    getLogger().info("Disabling Color Screen!");
+    pauseClrCont->SetActive(false);
+    }
+    else if (getMainConfig().lightsEnable.GetValue()) {}
+}
+MAKE_HOOK_MATCH(NoteCutParticlesEffect_SpawnParticles, &GlobalNamespace::NoteCutParticlesEffect::SpawnParticles, void, GlobalNamespace::NoteCutParticlesEffect* self, UnityEngine::Vector3 cutPoint, UnityEngine::Vector3 cutNormal, UnityEngine::Vector3 saberDir, float saberSpeed, UnityEngine::Vector3 noteMovementVec, UnityEngine::Color32 color, int sparkleParticlesCount, int explosionParticlesCount, float LifetimeMultiplier
+){
+
+    if(getMainConfig().NoParticles.GetValue())
+    {
+        sparkleParticlesCount = 0;
+        explosionParticlesCount = 0;
+        NoteCutParticlesEffect_SpawnParticles(self, cutPoint, cutNormal, saberDir, saberSpeed, noteMovementVec, color, sparkleParticlesCount,explosionParticlesCount,LifetimeMultiplier);
+    }
+    NoteCutParticlesEffect_SpawnParticles(self, cutPoint, cutNormal, saberDir, saberSpeed, noteMovementVec, color, sparkleParticlesCount,explosionParticlesCount,LifetimeMultiplier);
+
+};
+
+MAKE_HOOK_MATCH(BombExplosionEffect_SpawnExplosion, &GlobalNamespace::BombExplosionEffect::SpawnExplosion, void, GlobalNamespace::BombExplosionEffect* self, UnityEngine::Vector3 pos
+){
+
+    if(getMainConfig().NoBombs.GetValue() == true)
+
+    {
+        self->debrisCount = 0;
+        self->explosionParticlesCount = 0;
+        BombExplosionEffect_SpawnExplosion(self, pos);
+    }
+    BombExplosionEffect_SpawnExplosion(self, pos);
+};
 
 MAKE_HOOK_MATCH(aasd_SaberBurnMarkArea_OnEnable, &GlobalNamespace::SaberBurnMarkArea::OnEnable, void, GlobalNamespace::SaberBurnMarkArea*self)
 {
@@ -113,8 +252,7 @@ MAKE_HOOK_MATCH(AdvertisementButton, &GlobalNamespace::MainMenuViewController::D
     AdvertisementButton(self, firstActivation, addedToHierarchy, screenSystemEnabling); 
     
     
-    UnityEngine::UI::Button*PromotionButton = self->dyn__musicPackPromoButton();
-    UnityEngine::GameObject *gameObject = PromotionButton->get_gameObject();
+    auto gameObject = self->musicPackPromoBanner->get_gameObject();
     
     if(getMainConfig().NoPromo.GetValue() == true){
         gameObject->SetActive(false);
@@ -248,8 +386,8 @@ extern "C" void load() {
     getMainConfig().Init(modInfo);
     custom_types::Register::AutoRegister();
     QuestUI::Init();
-    QuestUI::Register::RegisterModSettingsViewController(modInfo, DidActivate);
-    QuestUI::Register::RegisterMainMenuModSettingsViewController(modInfo, DidActivate);
+    QuestUI::Register::RegisterModSettingsViewController<MoreMainUI *>(modInfo, "More By Core");
+    QuestUI::Register::RegisterMainMenuModSettingsViewController<MoreMainUI *>(modInfo, "More By Core");
     getLogger().info("Installing hooks...");
     // Install our hooks (none defined yet)
     INSTALL_HOOK(getLogger(), aasd_SaberBurnMarkArea_OnEnable);
@@ -260,5 +398,9 @@ extern "C" void load() {
     INSTALL_HOOK(getLogger(), LightsUpdater);
     INSTALL_HOOK(getLogger(), Song_select_exit);
     INSTALL_HOOK(getLogger(), ResultsScreenUI_didactivate);
+    INSTALL_HOOK(getLogger(), NoteCutParticlesEffect_SpawnParticles);
+    INSTALL_HOOK(getLogger(), BombExplosionEffect_SpawnExplosion);
+    INSTALL_HOOK(getLogger(), PauseMenuManager_ContinueButtonPressed);
+    INSTALL_HOOK(getLogger(), PauseMenuManager_ShowMenu);
     getLogger().info("Installed all hooks!");
 }
